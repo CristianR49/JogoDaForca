0a3589c968111a6e494036efa82286aeae8df608
Cristian <cristian.rr99@gmail.com>
2023-03-26 21:31:01 -0300

    Refatoração

--- Program.cs
+++ Program.cs
@@ -8,82 +8,61 @@ namespace JogoDaForca
              "MARACUJÁ", "MURICI", "PEQUI", "PITANGA", "PITAYA", "SAPOTI", "TANGERINA", "UMBU", "UVA", "UVAIA"};
         static string desenhoDaForca = "_____________\n|/           |\n|            |\n|            \n|            \n|            \n|\n|\n|\n|____\n";
         static Random numeroAleatorio;
-        static int chances = 5;
+        static int chances;
         static string palavraAleatoria;
-        static string[] letrasAcertadas;
+        static string[] letrasMisteriosas;
         static string letrasErradas;
-        static int acertos = 0;
+        static int acertos;
+        static bool acerto;
         static bool venceu;
+        static string chute;
+        static int i;
         static void Main(string[] args)
         {
 
-            ResetDasVariaveis();
+            ResetarAsVariaveis();
 
             while (true)
             {
                 Console.Clear();
 
-                Console.WriteLine("------------------Jogo da forca: Frutas------------------");
-                Console.WriteLine(desenhoDaForca);
-                for (int i = 0; i < letrasAcertadas.Length; i++)
-                {
-                    Console.Write(letrasAcertadas[i]);
-                }
-                Console.WriteLine($"\n\nLetras Erradas:{letrasErradas}");
+                MostrarLobbyDoJogo();
 
-                if (venceu == true)
+                if (venceu == true || chances == 0)
                 {
                     Console.ReadLine();
-                    ResetDasVariaveis();
+                    ResetarAsVariaveis();
                     continue;
                 }
 
-                if (chances == 0)
-                {
-                    ResetDasVariaveis();
-                    Console.ReadLine();
-                    continue;
-                }
+                InputarChute();
 
-                Console.WriteLine("\n\nDe seu chute: ");
-                string chute = Console.ReadLine().ToUpper();
-
-                bool acerto = false;
-                for (int i = 0; i < palavraAleatoria.Length; i++)
-                {
-                    if (chute == Convert.ToString(palavraAleatoria[i]) && letrasAcertadas[i] == "_")
-                    {
-                        letrasAcertadas[i] = Convert.ToString(palavraAleatoria[i]);
-                        acerto = true;
-                        acertos++;
-                        if (acertos == palavraAleatoria.Length)
-                        {
-                            desenhoDaForca = desenhoDaForca + "\nParabéns, você acertou!!";
-                            venceu = true;
-                        }
-                    }
-                    else if (i == palavraAleatoria.Length - 1 && acerto == false)
-                    {
-                        letrasErradas = letrasErradas + " " + chute;
-                        DesenharNaForcaOsPontosPerdidosEPerder();
-                    }
-
-                }
+                ProcessarAcertosEVitoriaOuErrosEDerrota();
 
             }
-
-            //comparar cada linha de array com o valor do chute: for, palavra[]
-            //se =, substitui no valor pos do array de string definido como _____
+            #region
+            //comparar cada linha do array da palavra misteriosa com o valor do chute: for, palavra[]
+            //se for igual, substitui na posição certa a letra da palavra, no array de string da palavra misteriosa definido como _____
+            #endregion
         }
-
-        private static void ResetDasVariaveis()
+        private static void MostrarLobbyDoJogo()
+        {
+            Console.WriteLine("------------------Jogo da forca: Frutas------------------");
+            Console.WriteLine(desenhoDaForca);
+            for (i = 0; i < letrasMisteriosas.Length; i++)
+            {
+                Console.Write(letrasMisteriosas[i]);
+            }
+            Console.WriteLine($"\n\nLetras Erradas:{letrasErradas}");
+        }
+        private static void ResetarAsVariaveis()
         {
             numeroAleatorio = new Random();
             palavraAleatoria = palavras[numeroAleatorio.Next(0, 28)];
-            letrasAcertadas = new string[palavraAleatoria.Length];
-            for (int i = 0; i < letrasAcertadas.Length; i++)
+            letrasMisteriosas = new string[palavraAleatoria.Length];
+            for (i = 0; i < letrasMisteriosas.Length; i++)
             {
-                letrasAcertadas[i] = "_";
+                letrasMisteriosas[i] = "_";
             }
             letrasErradas = "";
             desenhoDaForca = "_____________\n|/           |\n|            |\n|            \n|            \n|            \n|\n|\n|\n|____\n";
@@ -92,7 +71,41 @@ namespace JogoDaForca
             acertos = 0;
         }
 
-        static void DesenharNaForcaOsPontosPerdidosEPerder()
+        private static void InputarChute()
+        {
+            Console.WriteLine("\nDe seu chute: ");
+            chute = Console.ReadLine().ToUpper();
+        }
+        private static void ProcessarAcertosEVitoriaOuErrosEDerrota()
+        {
+            acerto = false;
+            for (i = 0; i < palavraAleatoria.Length; i++)
+            {
+                if (chute == Convert.ToString(palavraAleatoria[i]) && letrasMisteriosas[i] == "_")
+                {
+                    AcertosEVitoria();
+                }
+                else if (i == palavraAleatoria.Length - 1 && acerto == false)
+                {
+                    letrasErradas = letrasErradas + " " + chute;
+                    DesenharNaForcaOsPontosPerdidosEDerrota();
+                }
+            }
+        }
+        private static void AcertosEVitoria()
+        {
+            letrasMisteriosas[i] = Convert.ToString(palavraAleatoria[i]);
+            acerto = true;
+            acertos++;
+
+            if (acertos == palavraAleatoria.Length)
+            {
+                desenhoDaForca = desenhoDaForca + "\nParabéns, você acertou!!";
+                venceu = true;
+            }
+        }
+
+        static void DesenharNaForcaOsPontosPerdidosEDerrota()
         {
             chances--;
             if (chances == 4)
@@ -109,11 +122,11 @@ namespace JogoDaForca
             }
             if (chances == 1)
             {
-                desenhoDaForca = "_____________\n|/           |\n|            |\n|            o\n|           /|/\n|            \n|\n|\n|\n|____\n";
+                desenhoDaForca = "_____________\n|/           |\n|            |\n|            o\n|           /|\\ \n|            \n|\n|\n|\n|____\n";
             }
             if (chances == 0)
             {
-                desenhoDaForca = "_____________\n|/           |\n|            |\n|            o\n|           /|/\n|            |\n|\n|    Voce Perdeu!\n|\n|____\n\nPalavra Misteriosa: " + palavraAleatoria;
+                desenhoDaForca = "_____________\n|/           |\n|            |\n|            o\n|           /|\\ \n|            |\n|\n|    Voce Perdeu!\n|\n|____\n\nPalavra Misteriosa: " + palavraAleatoria;
             }
         }
     }
